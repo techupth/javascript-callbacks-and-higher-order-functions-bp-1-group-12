83cf2b56371f13db85d2ffb3535838c7326f3d4e
Kawin <kawind7@gmail.com>
2023-10-08 23:49:37 +0700

    Callbacks & Higher Order Functions #1-2

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,13 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 70) {
+      count++;
+    }
+  }
+  return count >= 5;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +17,9 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3);
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
