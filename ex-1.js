d0ad43ad6c954632c98e7204ad3e9b313384f3d5
Natakorn Santhip <1st.natakorn@gmail.com>
2023-10-10 18:30:08 +0700

    finish Assignment: Callbacks and Higher Order Functions

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,18 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let i = 0; i < array.length; i++) {
+    let newSalary = array[i] + 5000;
+    newEmployeeSalaries.push(newSalary);
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+forEach(employeeSalaries, function(salary) {
+  return salary;
+});
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
